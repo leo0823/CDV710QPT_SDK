1489dd212ae220306b9a6c28cee006d09c16990b
leo0823 <1320919868@qq.com>
2023-09-11 17:20:30 +0800

    适配onvif 19.12

--- indoor/app/share/onvif/onvif_user_token.h
+++ indoor/app/share/onvif/onvif_user_token.h
@@ -2,56 +2,17 @@
 #define _ONVIF_USER_TOKEN_H_
 #include <stdbool.h>
 #include <stdlib.h>
-/****************************************************************
-**@日期: 2022-09-20
-**@作者: leo.liu
-**@功能:获取随机数及base64decode 的随机数
-*****************************************************************/
-size_t onvif_random_create(char *random, char *random_base64_decode, int string_len);
-/****************************************************************
-**@日期: 2022-09-17
-**@作者: leo.liu
-**@功能:获取格林标准时间
-*****************************************************************/
-size_t onvif_time_create(char *created, int len);
-/****************************************************************
-**@日期: 2022-09-20
-**@作者: leo.liu
-**@功能:获取密码令牌
-*****************************************************************/
-bool onvif_usertoken_crate(const unsigned char *base64dec_romdom, size_t base64dec_romdom_len,
-                           const unsigned char *time, size_t time_len,
-                           const unsigned char *password, size_t pssword_len,
-                           char *token, size_t *token_len);
-/****************************************************************
- **@日期: 2022-09-20
- **@作者: leo.liu
- **@功能:格式化rtspxml
- *****************************************************************/
-size_t onvif_stream_url_xml_format(char *xml, const char *ip, const char *user, const char *password, const char *profile_token);
+
 /****************************************************************
 **@日期: 2022-09-21
 **@作者: leo.liu
-**@功能:profile token
+**@功能: 口令格式获取
 *****************************************************************/
-size_t onvif_user_token_xml_format(char *file, const char *ip, const char *user, const char *password);
+size_t onvif_xml_fomrat_by_token(char *data, const char *ip, const char *user, const char *password, int argc, char **argv);
 /****************************************************************
-**@日期: 2022-09-20
+**@日期: 2022-09-21
 **@作者: leo.liu
-**@功能: 编辑类型： register/delete
+**@功能: 摘要格式获取
 *****************************************************************/
-size_t onvif_edit_url_xml_format(char *file, const char *ip, const char *user, const char *password, const char *loc_sip_uri, const char *type);
-/****************************************************************
-**@日期: 2022-09-20
-**@作者: leo.liu
-**@功能: 编辑类型：set scopes
-*****************************************************************/
-size_t onvif_set_scopes_url_xml_format(char *file, const char *ip, const char *user, const char *password, const char *name, const char *type);
-/****************************************************************
-**@日期: 2022-09-20
-**@作者: leo.liu
-**@功能: 编辑类型：get scopes
-*****************************************************************/
-size_t onvif_get_scopes_url_xml_format(char *file, const char *ip, const char *user, const char *password, const char *type);
-
+size_t onvif_xml_fomrat_by_digest(char *data, const char *ip, const char *user, const char *password, const char *math, const char *uri, const char *qop, const char *realm, const char *nonce, int argv, char **argc);
 #endif
